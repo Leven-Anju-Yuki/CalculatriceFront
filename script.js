0fdb7abc7bef8ff3f854075eab188d47a1550602
Leven-Anju-Yuki <florie.decitre.60@gmail.com>
2024-02-14 13:54:37 +0100

    creation de la partie historique

--- script.js
+++ script.js
@@ -1,22 +1,69 @@
-const resultat=document.querySelector('.affichage');
+const resultat = document.querySelector(".affichage");
 function resoudre() {
-    let egal=eval(resultat.innerHTML);
-    resultat.innerHTML=egal;
+    let egal = eval(resultat.innerHTML);
+    resultat.innerHTML = egal;
 }
 //permet de faire un pourcentage
 function pourcentage() {
-    resultat.innerHTML=resultat.innerHTML/100;
+    resultat.innerHTML = resultat.innerHTML / 100;
 }
 //permet de tt effacer
 function effacer() {
-    resultat.innerHTML='0';
+    resultat.innerHTML = "0";
 }
 //enleve le zero des le depart
 function virezlezero() {
-    (resultat.innerHTML==0)? resultat.innerHTML=" ":resultat.innerHTML;
+    resultat.innerHTML == 0 ? (resultat.innerHTML = " ") : resultat.innerHTML;
 }
 //permet de se servir des touches de la calculatrice
 function afficher(touche) {
     virezlezero();
-    resultat.innerHTML+=touche;
+    resultat.innerHTML += touche;
 }
+
+//======================================================
+// POUR AVOIR L'HISTORIQUE DES CALCULS
+//======================================================
+
+const historique = document.getElementById("historique");
+
+function resoudre() {
+    let calcul = resultat.innerHTML;
+    let egal = eval(calcul);
+    resultat.innerHTML = egal;
+    ajouterHistorique(calcul, egal);
+}
+
+function ajouterHistorique(calcul, resultat) {
+    const maintenant = new Date();
+    //------------------------------------
+    //pour mettre l'heure entre chaque calcul
+    //------------------------------------
+    const options = {
+        timeZone: "Europe/Paris", 
+        hour: "numeric",
+        minute: "numeric",
+    };
+    const heureFrance = maintenant.toLocaleTimeString("fr-FR", options);
+    if (historique.innerHTML === "") {
+        historique.innerHTML = "<div><h4>Pas encore de calcul fait</h4></div>";
+    }
+    historique.innerHTML += `<div><h4>${heureFrance}<h4><p><b>${calcul}</b> = ${resultat} </p></div>`;
+}
+//======================================================
+// POUR switch entre les bouton ou le pads numerique
+//======================================================
+
+// const modeBtn = document.getElementById('mode');
+// let modeClavier = true;
+
+// modeBtn.addEventListener('click', () => {
+//     modeClavier = !modeClavier;
+//     if (modeClavier) {
+//         modeBtn.textContent = 'Utiliser le clavier';
+//     } else {
+//         modeBtn.textContent = 'Utiliser le pavé numérique';
+//     }
+// });
+
+
